8e73f359c40623285a2c85bfc86d51b379cf5b62
PraveenkumarElanchezhian <epraveenkumar1997@gmail.com>
2021-11-26 17:19:03 +0530

    load the Indian States Census Information from a csv

---
+++ src/practiceProblem/StateCSVModel.java
@@ -0,0 +1,23 @@
+package practiceProblem;
+
+public class StateCSVModel {
+//	public String SerialNo;
+//	public String StateName;
+//
+//	public String getSerialNo() {
+//		return SerialNo;
+//	}
+//
+//	public void setSerialNo(String serialNo) {
+//		SerialNo = serialNo;
+//	}
+//
+//	public String getStateName() {
+//		return StateName;
+//	}
+//
+//	public void setStateName(String stateName) {
+//		StateName = stateName;
+//	}
+//
+}
---
+++ src/practiceProblem/StatesCensus.java
@@ -0,0 +1,30 @@
+package practiceProblem;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class StatesCensus {
+
+	public void statesCensus() throws FileNotFoundException {
+		List<List<String>> input = new ArrayList<List<String>>();
+		Scanner userInput = new Scanner(new File("StateNames.csv"));
+		while (userInput.hasNextLine()) {
+			Scanner lineSc = new Scanner(userInput.nextLine()).useDelimiter("\t");
+			List<String> line = new ArrayList<String>();
+			while (lineSc.hasNext()) {
+				line.add(lineSc.next());
+			}
+			input.add(line);
+		}
+		System.out.println(input);
+	}
+
+	public static void main(String[] args) throws FileNotFoundException {
+		StatesCensus checkStatesCensus = new StatesCensus();
+		checkStatesCensus.statesCensus();
+
+	}
+}
